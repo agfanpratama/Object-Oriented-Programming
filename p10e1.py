9208d814a254e0db32005cb209a8b6607cf47d21
Agfan Herru Pratama <107943090+agfanpratama@users.noreply.github.com>
2024-05-17 14:04:15 +0700

    Praktikum Modul 10

---
+++ p10e1.py
@@ -0,0 +1,16 @@
+from bangun_datar import Persegi, Segitiga, Lingkaran, PersegiPanjang, JajarGenjang
+
+# Contoh penggunaan
+persegi = Persegi(7)
+segitiga = Segitiga(6, 8)
+lingkaran = Lingkaran(10)
+persegi_panjang = PersegiPanjang(7, 4)
+jajar_genjang = JajarGenjang(6, 8, 10)
+
+print("=============================\nNama: Agfan Herru Pratama H\nNim: 064002100043\n=============================")
+
+print("Luas persegi:", persegi.hitung_luas())
+print("Luas segitiga:", segitiga.hitung_luas())
+print("Luas lingkaran:", lingkaran.hitung_luas())
+print("Luas persegi panjang:", persegi_panjang.hitung_luas())
+print("Luas jajar genjang:", jajar_genjang.hitung_luas())
