a3ab8351c24960a98bd6eb221cc51782cb760537
Agfan Herru Pratama <107943090+agfanpratama@users.noreply.github.com>
2024-05-24 16:07:40 +0700

    p13.py

--- p13e1.py
+++ p13e1.py
@@ -26,36 +26,21 @@ class CustomWidget(QWidget):
         self.button = QPushButton("Kirim")
         self.button.setFont(QFont('Arial'))
 
-        self.reset_button = QPushButton("Reset")
-        self.reset_button.setFont(QFont('Arial'))
 
         self.layout.addWidget(self.label)
         self.layout.addWidget(self.name_textbox)
         self.layout.addWidget(self.nim_textbox)
         self.layout.addWidget(self.hobby_textbox)
         self.layout.addWidget(self.button)
-        self.layout.addWidget(self.reset_button)
         # self.setStyleSheet("background-color: #F5B7AA;")
         self.button.clicked.connect(self.greet)
-        self.reset_button.clicked.connect(self.reset)
         self.button.setStyleSheet("background-color: #82E0AA ;")
-        self.reset_button.setStyleSheet("background-color: #E9F5AA;")
 
     def greet(self):
         name = self.name_textbox.text()
         nim = self.nim_textbox.text()
         hobby = self.hobby_textbox.text()
-
-        if not name and not nim and not hobby:
-            QMessageBox.warning(self, "Peringatan", "Semua field harus diisi!")
-        elif not name:
-             QMessageBox.warning(self, "Peringatan", "Nama Belum diisi!")
-        elif not nim:
-             QMessageBox.warning(self, "Peringatan", "Nim Belum diisi!")
-        elif not hobby:
-             QMessageBox.warning(self, "Peringatan", "Hobby Belum diisi!")
-        else:
-            self.label.setText(f"Halo, {name}!\nNIM Anda adalah {nim}\ndan hobi Anda adalah {hobby}.")
+        self.label.setText(f"Halo, {name}!\nNIM Anda adalah {nim}\ndan hobi Anda adalah {hobby}.")
 
     def reset(self):
         self.name_textbox.clear()
